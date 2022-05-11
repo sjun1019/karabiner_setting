81e12dbac338b4ee942e359c71b592f8f963fbd4
쇼옴유저 <sjun1019@gmail.com>
2022-05-11 23:17:35 +0900

    complex modi install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+echo //////////////////////////////////////////////////
+echo     Install karabiner complex modi by sjun1019
+echo //////////////////////////////////////////////////
+
+cp -r ./karabiner.json ~/.config/karabiner/karabiner.json
