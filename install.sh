25e0a8dae475e9daf19e069d657f7c7245e1f925
쇼옴유저 <sjun1019@gmail.com>
2022-05-11 23:19:19 +0900

    no message

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ echo     Install karabiner complex modi by sjun1019
 echo //////////////////////////////////////////////////
 
 cp -r ./karabiner.json ~/.config/karabiner/karabiner.json
+
+echo Install succeed
